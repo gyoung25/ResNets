b10f86432549e4fe0b4b459791207dc3d0249aad
gyoung25 <131805131+gyoung25@users.noreply.github.com>
2024-03-26 14:15:52 -0400

    Update ResNets.py

--- ResNets.py
+++ ResNets.py
@@ -1,30 +1,46 @@
 '''
+This code is based on an assignment from the DeepLearning.AI's Coursera course titled Convolutional Neural 
+Networks. I have expanded on the assignment by adding functions to facilitate model evaluation and 
+generalizing the architecture to include any number of layers with or without skip connections and with or 
+without batch normalization.
 
-This code is based on an assignment from the DeepLearning.AI's Coursera course titled Convolutional Neural Networks. I have expanded on the assignment by adding functions to facilitate model evaluation and generalizing the architecture to include any number of layers with or without skip connections and with or without batch normalization.
-
-**Skills demonstrated:** multi-classification, tensorflow, ResNet architecture, model evaluation, general coding
+Skills demonstrated: multi-classification, tensorflow, ResNet architecture, model evaluation, general coding
 
 Overview: 
-> ResNet architecture (He et al, 2015) famously revolutionized deep learning by elegantly addressing the issue of vanishing gradients in very deep networks by adding skip connections. This code builds a deep neural network using ResNet architecture. For the sake of comparison with pre-ResNet architectures, I include helper functions that easily allow the removal of skip connections. The model is trained on images of hands holding up 0-5 fingers and returns an integer corresponding to the gesture in each image.
+    ResNet architecture (He et al, 2015) famously revolutionized deep learning by elegantly addressing the
+    issue of vanishing gradients in very deep networks by adding skip connections. This code builds a deep
+    neural network using ResNet architecture. For the sake of comparison with pre-ResNet architectures, I
+    include helper functions that easily allow the removal of skip connections. The model is trained on images
+    of hands holding up 0-5 fingers and returns an integer corresponding to the gesture in each image.
 
-Models:
-> - Images are initially fed into an input layer, then through a ZeroPadding layer, a Conv2D layer, BatchNormalization, ReLU activation, then a MaxPool layer. From there, output is passed through a prescribed number of what I'll call ResNet blocks. Each ResNet block contains a convolutional block and a prescribed number of identity blocks.
->- The convolutional blocks pass the input through three Conv2D layers with BatchNormalization and ReLU activation between, then pass the block input through a Conv2D and BatchNorm and adds it to the output of the third Conv2D layer before passing that sum through a ReLU activation. These blocks can change the dimension of the data.
->- The identity blocks similarly pass the input through three Conv2D layers with BatchNormalization and ReLU activation between, but simply adds the output of the third Conv2D layer to the block input without transforming the input first. These blocks maintain data dimensions.
->- After the last ResNet block, the output is passed through an AveragePooling2D layer, then a Flatten layer, then a Dense layer with softmax activation for prediction.
+Model(s):
+    - Images are initially fed into an input layer, then through a ZeroPadding layer, a Conv2D layer, 
+      BatchNormalization, ReLU activation, then a MaxPool layer. From there, output is passed through a
+      prescribed number of what I'll call ResNet blocks. Each ResNet block contains a convolutional block 
+      and a prescribed number of identity blocks.
+    - The convolutional blocks pass the input through three Conv2D layers with BatchNormalization and ReLU
+      activation between, then pass the block input through a Conv2D and BatchNorm and adds it to the output of
+      the third Conv2D layer before passing that sum through a ReLU activation. These blocks can change the
+      dimension of the data.
+    - The identity blocks similarly pass the input through three Conv2D layers with BatchNormalization and
+      ReLU activation between, but simply adds the output of the third Conv2D layer to the block input without
+      transforming the input first. These blocks maintain data dimensions.
+    - After the last ResNet block, the output is passed through an AveragePooling2D layer, then a Flatten layer,
+      then a Dense layer with softmax activation for prediction.
+    * BatchNormalization and skip connections can be removed using the corresponding boolean arguments in the
+      resNet_builder function
 
 Purpose: 
-> Demonstrate the effectiveness of skip connections in addressing the issue of vanishing gradients in very deep neural networks.
+- Demonstrate the effectiveness of skip connections in addressing the issue of vanishing gradients in very 
+  deep neural networks.
 
 Datasets: 
-
-> SIGNS dataset from DeepLearning.AI's Convolutional Neural Networks course. The dataset consists of 1080 training images and 120 test images of hands gesturing 0 through 5.
+- SIGNS dataset from DeepLearning.AI's Convolutional Neural Networks course. The dataset consists of 1080 
+  training images and 120 test images of hands gesturing 0 through 5.
     
 
 Target variable:
-
-> The number of digits held up in each image (0-5).
-
+- The number of digits held up in each image (0-5).
 '''
 
 import os
